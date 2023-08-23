839826f76ed38c92e6937eac0e1f3e19c61212f8
rushiraval3443 <138576324+rushiraval3443@users.noreply.github.com>
2023-08-23 12:27:26 +0530

    Done for the day

--- prac11/app/src/main/java/com/example/prac11/RegisterActivity.java
+++ prac11/app/src/main/java/com/example/prac11/RegisterActivity.java
@@ -8,8 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
