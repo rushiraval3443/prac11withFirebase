4b8fca23a1d6388cdf6e9f4918e7dcafc21b8a00
rushiraval3443 <138576324+rushiraval3443@users.noreply.github.com>
2023-08-29 16:46:05 +0530

    Rushi Commit 29-08-2023

--- prac11/app/src/main/java/com/example/prac11/RegisterActivity.java
+++ prac11/app/src/main/java/com/example/prac11/RegisterActivity.java
@@ -24,8 +24,6 @@ public class RegisterActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser mUser;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
